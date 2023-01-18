d515b9d3c961dfe276c08c818fa6e142b3249ee3
pasyagitka <lizavetazinovich@gmail.com>
2023-01-18 11:36:10 +0300

    fix:

--- src/brigadier/dto/update-brigadier.dto.ts
+++ src/brigadier/dto/update-brigadier.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateBrigadierDto } from './create-brigadier.dto';
 
 export class UpdateBrigadierDto extends PartialType(CreateBrigadierDto) {}
