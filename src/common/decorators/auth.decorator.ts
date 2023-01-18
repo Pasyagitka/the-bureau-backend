9fdb9230ec8005faf29697a89f56c22713e9069f
pasyagitka <lizavetazinovich@gmail.com>
2023-01-18 14:24:22 +0300

    feat: add 403 to response decorators

--- src/common/decorators/auth.decorator.ts
+++ src/common/decorators/auth.decorator.ts
@@ -1,10 +1,11 @@
-import { applyDecorators } from "@nestjs/common";
-import { ApiBearerAuth, ApiResponse } from "@nestjs/swagger";
-import { ErrorMessageResponseDto } from "../dto/error-message-response.dto";
+import { applyDecorators } from '@nestjs/common';
+import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
+import { ErrorMessageResponseDto } from '../dto/error-message-response.dto';
 
 export function ApiAuth() {
-    return applyDecorators(
-        ApiBearerAuth(),
-        ApiResponse({status: 401, type: ErrorMessageResponseDto})
-    );
+  return applyDecorators(
+    ApiBearerAuth(),
+    ApiResponse({ status: 401, type: ErrorMessageResponseDto }),
+    ApiResponse({ status: 403, type: ErrorMessageResponseDto }),
+  );
 }
