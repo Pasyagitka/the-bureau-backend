8bce8fcc487a2747354f5f8ec03de3733c2bccbe
Lizaveta Zinovich <zinovich_e@lwo.by>
2023-02-08 15:52:19 +0300

    refactor: change db container name

--- database/create.bat
+++ database/create.bat
@@ -1,5 +1,5 @@
-docker run --name some-postgres -p 5434:5432 -e POSTGRES_PASSWORD=postgres -d postgres
-docker exec some-postgres dir
-docker cp ./initial.sql some-postgres:/initial.sql
-docker exec some-postgres dir
-docker exec -it some-postgres psql -p 5432 -U postgres -d postgres -f initial.sql
+docker run --name thebureau-postgres -p 5434:5432 -e POSTGRES_PASSWORD=postgres -d postgres
+docker exec thebureau-postgres dir
+docker cp ./initial.sql thebureau-postgres:/initial.sql
+docker exec thebureau-postgres dir
+docker exec -it thebureau-postgres psql -p 5432 -U postgres -d postgres -f initial.sql
