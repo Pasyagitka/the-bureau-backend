1c54aeef3b8f11da30e47480fe04cca38bec7d29
Lizaveta Zinovich <lizavetazinovich@gmail.com>
2023-05-10 09:58:42 +0300

    refactor: remove isApproved from brigadier

--- src/brigadier/entities/brigadier.entity.ts
+++ src/brigadier/entities/brigadier.entity.ts
@@ -36,8 +36,8 @@ export class Brigadier {
   @Column('text', { name: 'avatarUrl', nullable: true })
   avatarUrl: string;
 
-  @Column('boolean', { name: 'isApproved', default: () => 'false' })
-  isApproved: boolean;
+  // @Column('boolean', { name: 'isApproved', default: () => 'false' })
+  // isApproved: boolean;
 
   @OneToOne(() => User, (user) => user.brigadier, { cascade: true, onDelete: 'CASCADE' })
   @JoinColumn([{ name: 'userId', referencedColumnName: 'id' }])
